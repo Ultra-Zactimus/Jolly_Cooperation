dd6f32bf3a8667cc13c790f6b7de506f9ef104e9
Ultra Zacky <ultrazacky@gmail.com>
2022-05-16 14:17:41 -0700

    add fetch to firestore in Games to diplay titles, attempt error message fix

--- src/components/Games.jsx
+++ src/components/Games.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row } from 'react-bootstrap';
+import { getDocs, collection } from 'firebase/firestore';
+import { db } from '../lib/init-firebase';
+import { v4 } from 'uuid';
 
 const Games = () => {
+  const [gameList, setGameList] = useState([]);
+  const gamesCollectionsRef = collection(db, "games");
+
+  useEffect(() => {
+    const getGames = async () => {
+      const data = await getDocs(gamesCollectionsRef);
+      setGameList(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+    };
+
+    getGames();
+  }, []);
+
   return (
     <Container>
       <Row>
@@ -13,139 +28,29 @@ const Games = () => {
                 <th scope="col" className="forum-col">
                   <h4>Title</h4>
                 </th>
-                <th scope="col">Platforms</th>
-                <th scope="col">Genre</th>
                 <th scope="col">Posts</th>
                 <th scope="col" className="last-post-col">Most Recent Post</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
+
+              {gameList.map((game) => {
+                return <>
+                  <tr>
                 <td>
-                  <h5 className="mb-0 h5"><a href="#">Destiny 2</a></h5>
+                  <h5 className="mb-0 h5" key={game.documentId}><a href="#">{game.title}</a></h5>
                 </td>
                 <td>
-                  <div>PC, XBOX, PlayStation</div>
-                </td>
-                <td>
-                  FPS
-                </td>
-                <td>
-                  50
+                  TBA
                 </td>
                 <td>
                   <h5 className="h6 mb-0"><a href="#">Title Name</a></h5>
                   <div>by <a href="#">username</a></div>
                 </td>
               </tr>
-              <tr>
-                <td>
-                  <h5 className="mb-0 h5"><a href="#">Warframe</a></h5>
-                </td>
-                <td>
-                  <div>PlayStation</div>
-                </td>
-                <td>
-                  Action
-                </td>
-                <td>
-                  30
-                </td>
-                <td>
-                  <h5 className="h6 mb-0"><a href="#">Title Name</a></h5>
-                  <div>by <a href="#">username</a></div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <h5 className="mb-0 h5"><a href="#">Animal Crossing: New Crossroads</a></h5>
-                </td>
-                <td>
-                  <div>Nintendo Switch</div>
-                </td>
-                <td>
-                  Adventure/Simulation
-                </td>
-                <td>
-                  12
-                </td>
-                <td>
-                  <h5 className="h6 mb-0"><a href="#">Title Name</a></h5>
-                  <div>by <a href="#">username</a></div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <h5 className="mb-0 h5"><a href="#">Dark Souls Remastered</a></h5>
-                </td>
-                <td>
-                  <div>PC, XBOX, PlayStation, Nintendo Switch</div>
-                </td>
-                <td>
-                  Action RPG
-                </td>
-                <td>
-                  22
-                </td>
-                <td>
-                  <h5 className="h6 mb-0"><a href="#">Title Name</a></h5>
-                  <div>by <a href="#">username</a></div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <h5 className="mb-0 h5"><a href="#">Street Fighter V</a></h5>
-                </td>
-                <td>
-                  <div>PC, PlayStation</div>
-                </td>
-                <td>
-                  Fighting
-                </td>
-                <td>
-                  33
-                </td>
-                <td>
-                  <h5 className="h6 mb-0"><a href="#">Title Name</a></h5>
-                  <div>by <a href="#">username</a></div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <h5 className="mb-0 h5"><a href="#">Gran Turismo 7</a></h5>
-                </td>
-                <td>
-                  <div>PlayStation</div>
-                </td>
-                <td>
-                  Driving Simulator
-                </td>
-                <td>
-                  20
-                </td>
-                <td>
-                  <h5 className="h6 mb-0"><a href="#">Title Name</a></h5>
-                  <div>by <a href="#">username</a></div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <h5 className="mb-0 h5"><a href="#">Elden Ring</a></h5>
-                </td>
-                <td>
-                  <div>PC, XBOX, PlayStation</div>
-                </td>
-                <td>
-                  Action RPG
-                </td>
-                <td>
-                  44
-                </td>
-                <td>
-                  <h5 className="h6 mb-0"><a href="#">Title Name</a></h5>
-                  <div>by <a href="#">username</a></div>
-                </td>
-              </tr>
+              </>
+              })}
+
             </tbody>
           </table>
         </div>
--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
 
+
+
 const MessageBoard = () => {
+
   return (
     <Container>
       <Row>
