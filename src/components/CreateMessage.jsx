89f4b4fbbe6c8068086a89f6dd26756724a349c1
Ultra Zacky <ultrazacky@gmail.com>
2022-05-16 16:50:02 -0700

    add function to create a new message in firestore

--- src/components/CreateMessage.jsx
+++ src/components/CreateMessage.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Form, Button } from 'react-bootstrap';
+import { db, auth } from '../lib/init-firebase';
+import { addDoc, collection } from 'firebase/firestore';
+import { useNavigate } from 'react-router-dom';
 
 const PostMessage = () => {
+
+  let navigate = useNavigate();
+
+  const [ title, setTitle ] = useState('');
+  const [ platform, setPlatform ] = useState('');
+  const [ description, setDescription ] = useState('');
+  const [ topicMessage, setTopicMessage ] = useState('');
+  const [ tags, setTags ] = useState('');
+  const [game, setGame] = useState('');
+
+  const messageCollectionRef = collection(db, "messages");
+
+  const createMessage = async () => {
+    await addDoc(messageCollectionRef, 
+      { 
+        title,
+        game, 
+        platform, 
+        description, 
+        topicMessage, 
+        tags,
+        author: { 
+          name: auth.currentUser.displayName, 
+          id: auth.currentUser.uid
+        }
+      });
+      navigate("/");
+  };
 
   return (
     <Container 
@@ -9,11 +40,20 @@ const PostMessage = () => {
       <Form>
         <h3 className="mt-5">Post a New Message</h3>
 
+        <Form.Label className="mt-5">Game</Form.Label>
+        <Form.Control 
+          className="mt-2"
+          name="game"
+          placeholder="Name of the Game"
+          onChange={(e) => setGame(e.target.value)}
+        />
+
         <Form.Label className="mt-5">Platform</Form.Label>
         <Form.Control 
           className="mt-2"
           name="platform"
           placeholder="Platform You Play This Game On"
+          onChange={(e) => setPlatform(e.target.value)}
         />
 
         <Form.Label className="mt-5">Topic Title</Form.Label>
@@ -21,6 +61,7 @@ const PostMessage = () => {
           className="mt-2"
           name="title"
           placeholder="Title of Your Post"
+          onChange={(e) => setTitle(e.target.value)}
         />
 
         <Form.Label className="mt-5">Topic Description</Form.Label>
@@ -28,6 +69,7 @@ const PostMessage = () => {
           className="mt-2"
           name="description"
           placeholder="Brief Description of Topic"
+          onChange={(e) => setDescription(e.target.value)}
         />
 
         <Form.Label className="mt-5">Your Message</Form.Label>
@@ -36,6 +78,7 @@ const PostMessage = () => {
           as="textarea"
           name="message"
           placeholder="Type Your Message Here"
+          onChange={(e) => setTopicMessage(e.target.value)}
         />
 
         <Form.Label className="mt-5">Your Gamer Tags</Form.Label>
@@ -43,12 +86,13 @@ const PostMessage = () => {
           className="mt-2"
           name="tags"
           placeholder="Your Gamer Tags"
+          onChange={(e) => setTags(e.target.value)}
         />
 
         <Button 
           className="btn btn-success mt-5 mb-5 shadow-sm"
           type="submit"
-        >Post Message</Button>
+          onClick={createMessage}>Post Message</Button>
       </Form>
     </Container>
   );
