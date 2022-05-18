933a8139b31438f6ee78eeafad1f885e9d7ac11d
Ultra Zacky <ultrazacky@gmail.com>
2022-05-18 14:47:43 -0700

    add change to layout of messageboard component

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -15,42 +15,19 @@ const MessageBoard = () => {
                 <th scope="col" className="forum-col">
                   <h4><strong>Multiplayer</strong></h4>
                 </th>
-                <th scope="col">Games</th>
-                <th scope="col">Posts</th>
-                <th scope="col" className="last-post-col">Last Post</th>
               </tr>
             </thead>
             <tbody>
               <tr>
                 <td>
-                  <h5 className="mb-0 h5"><a href="/games">Co-Op</a></h5>
+                  <h5 className="mb-0 h5"><a href="/message-list">Co-Op</a></h5>
                   <p className="mb-0">Forum for players to meet other players for co-op play</p>
-                </td>
-                <td>
-                  <div>9</div>
-                </td>
-                <td>
-                  30
-                </td>
-                <td>
-                  <h5 className="h6 mb-0"><a href="#">Title Name</a></h5>
-                  <div>by <a href="#">username</a></div>
                 </td>
               </tr>
               <tr>
                 <td>
-                  <h5 className="mb-0 h5"><a href="/games">PVP</a></h5>
+                  <h5 className="mb-0 h5"><a href="/message-list">PVP</a></h5>
                   <p className="mb-0">Forum for players to meet other players for competitive play</p>
-                </td>
-                <td>
-                  <div>9</div>
-                </td>
-                <td>
-                  30
-                </td>
-                <td>
-                  <h5 className="h6 mb-0"><a href="#">Author Name</a></h5>
-                  <div>by <a href="#">username</a></div>
                 </td>
               </tr>
             </tbody>
@@ -61,9 +38,6 @@ const MessageBoard = () => {
                 <th scope="col" className="forum-col">
                   <h4><strong>Guides</strong></h4>
                 </th>
-                <th scope="col">Games</th>
-                <th scope="col">Posts</th>
-                <th scope="col" className="last-post-col">Last Post</th>
               </tr>
             </thead>
             <tbody>
@@ -72,31 +46,11 @@ const MessageBoard = () => {
                   <h5 className="mb-0 h5"><a href="#">Walkthroughs</a></h5>
                   <p className="mb-0">Guides made by players to help players</p>
                 </td>
-                <td>
-                  <div>9</div>
-                </td>
-                <td>
-                  30
-                </td>
-                <td>
-                  <h5 className="h6 mb-0"><a href="#">Post Name</a></h5>
-                  <div>by <a href="#">Author Name</a></div>
-                </td>
               </tr>
               <tr>
                 <td>
                   <h5 className="mb-0 h5"><a href="#">Questions</a></h5>
                   <p className="mb-0">Ask the community a question about a game</p>
-                </td>
-                <td>
-                  <div>9</div>
-                </td>
-                <td>
-                  30
-                </td>
-                <td>
-                  <h5 className="h6 mb-0"><a href="#">Post Name</a></h5>
-                  <div>by <a href="#">Author Name</a></div>
                 </td>
               </tr>
             </tbody>
@@ -107,9 +61,6 @@ const MessageBoard = () => {
                 <th scope="col" className="forum-col">
                   <h4><strong>General Discussion</strong></h4>
                 </th>
-                <th scope="col">Games</th>
-                <th scope="col">Posts</th>
-                <th scope="col" className="last-post-col">Last Post</th>
               </tr>
             </thead>
             <tbody>
@@ -118,31 +69,11 @@ const MessageBoard = () => {
                   <h5 className="mb-0 h5"><a href="#">Game News</a></h5>
                   <p className="mb-0">dicussions on the latest gaming news</p>
                 </td>
-                <td>
-                  <div>9</div>
-                </td>
-                <td>
-                  30
-                </td>
-                <td>
-                  <h5 className="h6 mb-0"><a href="#">Post Name</a></h5>
-                  <div>by <a href="#">Author Name</a></div>
-                </td>
               </tr>
               <tr>
                 <td>
                   <h5 className="mb-0 h5"><a href="#">Polls</a></h5>
                   <p className="mb-0">participate in game related polls with other players</p>
-                </td>
-                <td>
-                  <div>9</div>
-                </td>
-                <td>
-                  30
-                </td>
-                <td>
-                  <h5 className="h6 mb-0"><a href="#">Post Name</a></h5>
-                  <div>by <a href="#">Author Name</a></div>
                 </td>
               </tr>
             </tbody>
@@ -153,9 +84,6 @@ const MessageBoard = () => {
                 <th scope="col" className="forum-col">
                   <h4><strong>Help</strong></h4>
                 </th>
-                <th scope="col">Games</th>
-                <th scope="col">Posts</th>
-                <th scope="col" className="last-post-col">Last Post</th>
               </tr>
             </thead>
             <tbody>
@@ -164,15 +92,11 @@ const MessageBoard = () => {
                   <h5 className="mb-0 h5"><a href="#">Troubleshooting</a></h5>
                   <p className="mb-0">troubleshoot technical problems</p>
                 </td>
+              </tr>
+              <tr>
                 <td>
-                  <div>9</div>
-                </td>
-                <td>
-                  30
-                </td>
-                <td>
-                  <h5 className="h6 mb-0"><a href="#">Post Name</a></h5>
-                  <div>by <a href="#">Author Name</a></div>
+                  <h5 className="mb-0 h5"><a href="#">FAQ</a></h5>
+                  <p className="mb-0">check our frequently asked questions</p>
                 </td>
               </tr>
             </tbody>
